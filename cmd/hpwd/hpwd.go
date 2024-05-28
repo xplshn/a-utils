7e42a5dc47a7a4168f3b90bc17a8472fbe85f007
xplshn <musicslave66@murena.io>
2024-05-28 14:46:04 -0300

    Added HPWD

---
+++ cmd/hpwd/hpwd.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting home directory:", err)
+		return
+	}
+
+	coolHome := os.Getenv("COOLHOME")
+	coolHomeDepth := os.Getenv("COOLHOME_DEPTH")
+
+	if pwd == home {
+		if coolHome != "" {
+			fmt.Print(coolHome)
+		} else {
+			fmt.Print("~") // Display '~' if in home directory
+		}
+	} else if strings.HasPrefix(pwd, home+"/") { // Adjusted condition
+		// Calculate the relative path from home
+		relativePath := pwd[len(home)+1:]
+		if coolHomeDepth != "" {
+			fmt.Printf("%s/%s ", coolHomeDepth, relativePath)
+		} else {
+			fmt.Printf("~/%s ", relativePath) // Display '~/...' for directories inside home if COOLHOME_DEPTH is not set
+		}
+	} else {
+		fmt.Print(pwd + " ") // Display the full path if outside home
+	}
+}
