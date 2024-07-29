86aaa271f7d6ef9384ab62fd23bdff208ec40293
xplshn <musicslave66@murena.io>
2024-07-29 18:57:58 -0300

    move file

--- cmd/demo/demo.go
+++ cmd/demo/demo.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, xplshn [3BSD]
+// Copyright (c) 2024-2024 xplshn                       [3BSD]
 // For more details refer to https://github.com/xplshn/a-utils
 package main
 
@@ -14,12 +14,12 @@ import (
 func main() {
 	// Create an instance of CmdInfo
 	cmdInfo := &ccmd.CmdInfo{
-		Authors:      []string{"John Doe", "Jane Smith"},
-		Name:         "example",
-		Synopsis:     "command [options]",
-		Description:  "This is an example command line tool to demonstrate the ccmd library.",
+		Authors:      []string{"John Doe", "Jane Smith", "Joe Momma"},
+		Name:         "demo",
+		Synopsis:     "demo [options]",
+		Description:  "This is an example command line tool to demonstrate the ccmd (consistent command line) library.",
 		Behavior:     "This tool demonstrates basic usage of the ccmd library and formatting.",
-		Notes:        "Make sure to use the correct flags and options as shown below.",
+		Notes:        "we good",
 		ExcludeFlags: make(map[string]bool),
 		Since:        1999,
 	}
@@ -46,15 +46,22 @@ func main() {
 
 	// Print the centered help page
 	fmt.Println(ccmd.FormatCenter("Centered text 1"))
+	header := "long text (centered)"
 	paragraph := `I am a very long paragraph, with more than one line and son
 I expect you not read this, its just to test the header title alignment`
-	header := "long text"
 	fmt.Println(ccmd.CFormatCenter(header, ccmd.RelativeTo(paragraph)))
 	fmt.Println(paragraph)
 	fmt.Println()
+	header = "long text, left-aligned (will rob you and tax you)"
 	paragraph = `I am a very long paragraph, with more than one line and son
 I expect you not read this, its just to test the header title alignment`
-	header = "long text, left-aligned (will rob you and tax you)"
 	fmt.Println(ccmd.CFormatLeft(header, ccmd.RelativeTo(paragraph)))
 	fmt.Println(paragraph)
+	fmt.Println()
+	header = "long text, right-aligned (will rob you and tax you, its not a replacement for a libertarian, but says it is)"
+	paragraph = `I am a very long paragraph, with more than one line and son
+I expect you not read this, its just to test the header title alignment`
+	fmt.Println(ccmd.CFormatRight(header, ccmd.RelativeTo(paragraph)))
+	fmt.Println(paragraph)
+	fmt.Println()
 }
