a94b5359d61129e06746346238239171c418bc74
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-09 19:41:48 -0300

    Update envsubst.go

--- cmd/envsubst/envsubst.go
+++ cmd/envsubst/envsubst.go
@@ -18,7 +18,7 @@ func main() {
 		Authors:     []string{"xplshn"},
 		Repository:  "https://github.com/xplshn/a-utils",
 		Description: "Substitutes environment variables in shell format strings",
-		Synopsis:    "[|VARIABLEs|]",
+		Synopsis:    "[SHELL_FORMAT]",
 		CustomFields: map[string]interface{}{
 			"1_Behavior": "By default, reads stdin and substitutes environment variables.",
 			"2_Examples":  `  \$ echo 'Hello $USER' | envsubst`,
